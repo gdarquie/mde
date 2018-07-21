ae841f8aacc69f05523dda85c36b631aebd61090
gdarquie <gaetan.darquie@gmail.com>
2018-07-21 11:28:53 +0200

    Page d'ajout des personnages

---
+++ src/app/components/personnage/add-personnage/add-personnage.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Personnage } from '../../../classes/personnage';
+import { PersonnageService } from '../../../services/personnage.service';
+import { HttpClient } from '@angular/common/http';
+import {ModalComponent} from '../../modal/modal.component';
+import {MatDialogRef} from '@angular/material';
+
+@Component({
+  selector: 'app-add-personnage',
+  templateUrl: './add-personnage.component.html',
+  styleUrls: ['./add-personnage.component.css']
+})
+export class AddPersonnageComponent implements OnInit {
+
+  constructor(
+    private http: HttpClient,
+    private personnageService: PersonnageService,
+    public dialogRef: MatDialogRef<ModalComponent>,
+  ) {}
+
+  personnage: Personnage;
+  personnages: Personnage[];
+  content: string;
+
+  @Input() fictionId;
+
+  ngOnInit() {
+  }
+
+  /**
+   * @param {string} titre
+   * @param {string} description
+   * @param {number} fiction
+   * @param {number} annee_naissance
+   * @param {number} annee_mort
+   */
+  addPersonnage(titre: string, description: string, fiction: number, annee_naissance?: number, annee_mort?: number): void {
+    titre = titre.trim();
+    description = description.trim();
+    if (!titre || !description) { return; }
+    console.log(fiction);
+    this.personnageService.addPersonnage({ titre, description, fiction, annee_naissance, annee_mort } as Personnage)
+      .subscribe(personnage => {
+        this.personnages.push(personnage);
+      });
+    this.dialogRef.close();
+  }
+
+}
