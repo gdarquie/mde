dd9ac14a71b5ce00763fde3cec79605de0fa69ed
gdarquie <gaetan.darquie@gmail.com>
2018-11-25 02:25:41 +0100

    change for CORS

--- src/app/services/projet.service.ts
+++ src/app/services/projet.service.ts
@@ -18,9 +18,6 @@ export class ProjetService {
     private httpPublicOptions = {
         headers: new HttpHeaders({
             'Content-Type':  'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
         })
     };
 
